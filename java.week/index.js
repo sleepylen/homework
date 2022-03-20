0982a838a486714a1e68cca2fb2f3f23207b4b35
sleepylen <iamfailure@mail.ru>
2022-03-20 23:46:30 +0700

    +

---
+++ java.week/index.js
@@ -0,0 +1,33 @@
+function showMessage() {
+    let a = prompt("Как тебя зовут?", 'твое прекрасное имя');
+    
+alert('Привет,' + a + '!');
+}
+
+function showAdd() {
+    let a = +prompt("Первое число?", '');
+    let b = +prompt("Второе число?", '');
+
+alert(a + b);
+}
+
+function showSub() {
+    let a = +prompt("Первое число?", '');
+    let b = +prompt("Второе число?", '');
+
+alert(a - b);
+}
+
+function showMulti() {
+    let a = +prompt("Первое число?", '');
+    let b = +prompt("Второе число?", '');
+
+alert(a * b);
+}
+
+function showDiv() {
+    let a = +prompt("Первое число?", '');
+    let b = +prompt("Второе число?", '');
+
+alert(a / b);
+}
